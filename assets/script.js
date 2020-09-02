167e1326ea6de5e5dc333b4b156492b9aece6160
Jude Clark <judeclark2019@gmail.com>
2020-09-02 19:08:22 -0400

    rebuilt randomizers

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Functions to generate random characters from the arrays
+// Used Character Codes https://www.w3schools.com/jsref/jsref_fromcharcode.asp
+function getRandomLowercase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97); //There are 26 lowercase letters and they start and number 97 in the UTF-8 decimal system http://www.net-comber.com/charset.html
+}
+
+function getRandomUppercase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol(){
+  const availableSymbols = "~`!@#$%^&*()-_=+[{]},<.>?" //I'm using an array of my own choices since they are all spread out from each other in CharCode
+  return availableSymbols[Math.floor(Math.random()*availableSymbols.length)];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +28,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// When user clicks the button, run function "writePassword"
 generateBtn.addEventListener("click", writePassword);
