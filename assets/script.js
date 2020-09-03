dad75004bf414f291cf7bee5903f177caac6143a
Jude Clark <judeclark2019@gmail.com>
2020-09-03 15:27:15 -0400

    Prompts user for password length, performs validation, if pass, print the value to the console.

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,28 @@ var lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbersString = "0123456789";
 var symbolsString = "`~!@#$%^&*()-_=+[{]};:,<.>?";
+
+//Password Generator Function
+function generatePassword() {
+  //ask the user for desired length
+  var desiredPwLength = Math.round(prompt("How many characters long would you like your password to be (between 8 and 128)?"));
+  //check that the input is within constrains
+  if (desiredPwLength>=8 && desiredPwLength <=128) {
+    var confirmedPwLength = desiredPwLength;
+    console.log("Password length: " + confirmedPwLength);
+  } else {
+
+  }
+}
+
+// When button is clicked
+function writePassword() {
+  //call the generatePassword function and print it
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
