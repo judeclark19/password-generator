35b0cb9f6dedd5a1fd63c98a366cc8274d40bb08
Jude Clark <judeclark2019@gmail.com>
2020-09-03 10:04:22 -0400

    Finished sandboxing. Ready to rebuild again.

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,19 @@ var generateBtn = document.querySelector("#generate");
 
 //Functions to generate random characters from the arrays
 // Used Character Codes https://www.w3schools.com/jsref/jsref_fromcharcode.asp
-function getRandomLowercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97); //There are 26 lowercase letters and they start and number 97 in the UTF-8 decimal system http://www.net-comber.com/charset.html
+
+//build lowercase array
+function buildLcArray() {
+  const lowerCaseArray = []
+    for (let i=97; i<=127; i++){
+      lowerCaseArray.push(i);
+    }
+
+    console.log(lowercaseArray);
 }
+// function getRandomLowercase() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97); //There are 26 lowercase letters and they start and number 97 in the UTF-8 decimal system http://www.net-comber.com/charset.html
+// }
 
 function getRandomUppercase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -20,7 +30,14 @@ function getRandomSymbol() {
   return availableSymbols[Math.floor(Math.random() * availableSymbols.length)];
 }
 
-// Write password to the #password input
+//this object groups all the randomizers together
+const randomizerFuncs = {
+  randLow: getRandomLowercase(),
+    randUp: getRandomUppercase(),
+    randNum: getRandomNumber(),
+    randSym: getRandomSymbol() }
+
+//When button is clicked,
 function writePassword() {
   //ask for desired length
   var desiredPwLength = prompt(
@@ -31,28 +48,39 @@ function writePassword() {
     console.log("Password length: " + desiredPwLength);
   } else {
     alert("Enter a number between 8 and 128 please.");
+    prompt(
+      "How many characters long would you like your password to be (between 8 and 128)?");
   }
 
   //next, ask if lowercase allowed.
-  var lowercaseAllowed = confirm("Use lowercase letters in your new password?");
+  const lowercaseAllowed = confirm("Use lowercase letters in your new password?");
   console.log("Use lowercase: " + lowercaseAllowed);
 
   //next, ask if uppercase allowed.
-  var uppercaseAllowed = confirm("Use upper case letters in your new password?");
+  const uppercaseAllowed = confirm("Use upper case letters in your new password?");
   console.log("Use uppercase: " + uppercaseAllowed);
 
   //next, ask if numbers allowed.
-  var numbersAllowed = confirm("Use numbers in your new password?");
-  console.log("Use numbers: " + numbersAllowed);
-
+  const numbersAllowed = confirm("Use numbers in your new password?");
+  console.log("Use numbers: " + numbersAllowed);s
   //next, ask if special characters allowed.
-  var symbolsAllowed = confirm("Use special characters in your new password?");
+  const symbolsAllowed = confirm("Use special characters in your new password?");
   console.log("Use special characters: " + symbolsAllowed);
 
+  //use generatePassword(); and then print it to the passwordTextArea
   var password = generatePassword();
   var passwordTextArea = document.querySelector("#password");
 
+  //(print)
   passwordTextArea.value = password;
+}
+
+//Generate Password function
+function generatePassword (desiredPwLength, lowercaseAllowed, uppercaseAllowed, numbersAllowed, symbolsAllowed) {
+  let availableCharactersArray
+
+
+
 }
 
 // When user clicks the button, run function "writePassword"
---
+++ assets/scriptver2.js
@@ -0,0 +1,88 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//four sets of character strings
+var lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbersString = "0123456789";
+var symbolsString = "`~!@#$%^&*()-_=+[{]};:,<.>?";
+
+//Password Generator Function
+function generatePassword() {
+  //ask the user for desired length
+  var desiredPwLength = Math.round(prompt(
+    "How many characters long would you like your password to be (between 8 and 128)?"
+  ));
+  //check that it is within constraints
+  if (desiredPwLength >= 8 && desiredPwLength <= 128) {
+    var confirmedPwLength = desiredPwLength;
+    console.log("Password length: " + confirmedPwLength);
+  } else {
+    alert("Enter a number between 8 and 128 please.");
+    throw new Error("User did not chose a length between 8 and 128 characters");
+  }
+
+  //next, ask if lowercase allowed.
+  const lowercaseAllowed = confirm(
+    "Use lowercase letters in your new password?"
+  );
+  console.log("Use lowercase: " + lowercaseAllowed);
+
+  //next, ask if uppercase allowed.
+  const uppercaseAllowed = confirm(
+    "Use upper case letters in your new password?"
+  );
+  console.log("Use uppercase: " + uppercaseAllowed);
+
+  //next, ask if numbers allowed.
+  const numbersAllowed = confirm("Use numbers in your new password?");
+  console.log("Use numbers: " + numbersAllowed);
+
+  //next, ask if special characters allowed.
+  const symbolsAllowed = confirm(
+    "Use special characters in your new password?"
+  );
+  console.log("Use special characters: " + symbolsAllowed);
+
+  //now, build the string of all available characters based on user answers
+  var allAvailableCharacters = "";
+  if (lowercaseAllowed === true) {
+    allAvailableCharacters = allAvailableCharacters.concat(lowerCaseString);
+  }
+  if (uppercaseAllowed === true) {
+    allAvailableCharacters = allAvailableCharacters.concat(upperCaseString);
+  }
+  if (numbersAllowed === true) {
+    allAvailableCharacters = allAvailableCharacters.concat(numbersString);
+  }
+  if (symbolsAllowed === true) {
+    allAvailableCharacters = allAvailableCharacters.concat(symbolsString);
+  }
+  console.log(allAvailableCharacters); //test pass
+
+  //now, pick a random character from the array we just built and put it in a new string for the number of times the user chose at the beginning
+
+  var newPassword = "";
+
+  for (let i = 0; i < confirmedPwLength; i++) {
+    newPassword = newPassword.concat(
+      allAvailableCharacters[
+        Math.floor(Math.random() * allAvailableCharacters.length)
+      ]
+    );
+  }
+
+  return newPassword;
+}
+
+// When button is clicked
+function writePassword() {
+  //call the generatePassword function and print it
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
